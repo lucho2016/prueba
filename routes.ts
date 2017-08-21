37035f448c44b56d2f01e416de14f6629a458f57
Luis Leal <lfl284@hotmail.com>
2017-08-21 20:01:54 -0300

    Agregamos comentario a la página

--- routes.ts
+++ routes.ts
@@ -1,3 +1,4 @@
+//comentario para ver
 import {Routes} from '@angular/router';
 import {LoginComponent} from './public/login/login.component';
 import { HomeComponent } from './auth/home/home.component';
