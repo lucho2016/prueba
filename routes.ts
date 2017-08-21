ef62fbafacd96e93bb503956b3ff881c11b692e4
Luis Leal <lfl284@hotmail.com>
2017-08-21 19:59:47 -0300

    Subi el primer archivo

---
+++ routes.ts
@@ -0,0 +1,26 @@
+import {Routes} from '@angular/router';
+import {LoginComponent} from './public/login/login.component';
+import { HomeComponent } from './auth/home/home.component';
+import {ProjectListComponent} from './auth/project-list/project-list.component';
+import {PublicGuard} from './common/guards/public.guard';
+import {AuthGuard} from './common/guards/auth.guard';
+import {NotFoundComponent} from './common/not-found/not-found.component';
+
+export const routes: Routes =  [
+  {
+    path: '', pathMatch : 'full', redirectTo : '/login'
+  },
+  {
+    path: 'login', component: LoginComponent, pathMatch: 'full', canActivate: [PublicGuard]
+  },
+
+  {
+    path: 'home', component: HomeComponent, canActivate: [AuthGuard]
+  },
+  {
+    path: 'proyects', component: ProjectListComponent, canActivate: [AuthGuard]
+  },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];
